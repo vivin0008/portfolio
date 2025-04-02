fbe3ba5af365a60a8b54fc4f576f33f831335432
vivin0008 <2322jb58@kgcas.com>
2025-04-02 22:43:11 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll("nav ul li a");
+    navLinks.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
+        });
+    });
+ });
